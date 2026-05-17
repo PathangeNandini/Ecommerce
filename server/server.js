7cf29885ebdbe4d31d277ce492250a509d415d8d
srinithi10052005 <srinithibaskaran456@gmail.com>
2026-05-17 19:27:18 +0530

    Fix server startup logic and update database connection import

--- server/server.js
+++ server/server.js
@@ -13,10 +13,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const authRoutes = require("./routes/auth.routes");
-
-app.use("/api/auth", authRoutes);
-
 app.get("/", (req, res) => {
   res.send("Food Delivery API Running");
 });
